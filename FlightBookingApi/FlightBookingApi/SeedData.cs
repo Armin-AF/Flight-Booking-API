31fa4e416501e54e97fee141ba854e363a7bcf73
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-03-08 10:00:20 +0100

    Revert "SeedData Setup"
    This reverts commit ef5c99ee2484c364f7d42dfb5bd6eb7a06764dfa.

--- FlightBookingApi/FlightBookingApi/SeedData.cs
+++ FlightBookingApi/FlightBookingApi/SeedData.cs
@@ -1,29 +1,12 @@
-using FlightBookingApi.DataBase;
 using FlightBookingApi.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json;
 
 namespace FlightBookingApi;
 
 public class SeedData : Migration
 {
-    protected override void Up(MigrationBuilder migrationBuilder)
-    {
-        // Read data from JSON file
-        string json = File.ReadAllText("data.json");
-        List<FlightRoute> flightRoutes = JsonConvert.DeserializeObject<List<FlightRoute>>(json);
-
-        // Create database context
-        var optionsBuilder = new DbContextOptionsBuilder<FlightDbContext>();
-        optionsBuilder.UseSqlServer("your_connection_string_here");
-        var dbContext = new FlightDbContext(optionsBuilder.Options);
-
-        // Add each FlightRoute to the DbSet and save changes
-        foreach (var flightRoute in flightRoutes)
-        {
-            dbContext.FlightRoutes.Add(flightRoute);
-        }
-        dbContext.SaveChanges();
+    
+    protected override void Up(MigrationBuilder migrationBuilder){
+        throw new NotImplementedException();
     }
 }
