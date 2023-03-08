605f4c8a51191301162cb8ca8b59681db9a77c26
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-03-08 10:00:26 +0100

    Revert "Seed data prep"
    This reverts commit 04a21c04f638b385e4bb880fec9ffb52394bcef8.

--- FlightBookingApi/FlightBookingApi/Models/Flight.cs
+++ FlightBookingApi/FlightBookingApi/Models/Flight.cs
@@ -1,22 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace FlightBookingApi.Models;
 
 public class Flight
 {
-    public virtual ICollection<Booking> bookings { get; set; }
-    [Required]
-    [Key]
+    public IEnumerable<Booking> bookings;
     public string flight_id { get; set; }
-    
-    [Required]
-    [DataType(DataType.DateTime)]
     public DateTime departureAt { get; set; }
-    [Required]
-    [DataType(DataType.DateTime)]
     public DateTime arrivalAt { get; set; }
     public int availableSeats { get; set; }
-    public virtual Price prices { get; set; }
+    public Price prices { get; set; }
     public TimeSpan layoverDuration{ get; set; } = TimeSpan.Zero;
 }
--- FlightBookingApi/FlightBookingApi/Models/FlightRoute.cs
+++ FlightBookingApi/FlightBookingApi/Models/FlightRoute.cs
@@ -1,12 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace FlightBookingApi.Models;
 
 public class FlightRoute
 {
-    [Key]
     public string route_id { get; set; }
     public string departureDestination { get; set; }
     public string arrivalDestination { get; set; }
-    public virtual ICollection<Flight> itineraries { get; set; }
+    public List<Flight> itineraries { get; set; }
 }
